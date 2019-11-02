6df8d074e9f673280cbabadc4c60bb0a62ae8ac7
drpradsms <dr.pradsms@gmail.com>
2019-11-02 23:36:50 +0530

    Revert "DeviceParts: OOS screen calibration settings"
    This reverts commit b43d48cc511e64a6fc4173cc1727fabfb2b43b05.
    
    Signed-off-by: drpradsms <dr.pradsms@gmail.com>

--- DeviceParts/src/org/omnirom/device/PanelSettings.java
+++ DeviceParts/src/org/omnirom/device/PanelSettings.java
@@ -30,9 +30,6 @@ import androidx.preference.PreferenceCategory;
 import androidx.preference.PreferenceManager;
 import androidx.preference.PreferenceScreen;
 import androidx.preference.TwoStatePreference;
-import android.support.v4.view.ViewPager;
-import android.support.v4.content.ContextCompat;
-import androidx.app.AppCompatActivity;
 import android.provider.Settings;
 import android.text.TextUtils;
 import android.view.LayoutInflater;
@@ -41,20 +38,13 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.util.Log;
 
-
 public class PanelSettings extends PreferenceFragment implements RadioGroup.OnCheckedChangeListener {
     private RadioGroup mRadioGroup;
-    ViewPager viewPager;
-    LinearLayout sliderDotspanel;
-    private int dotscount;
-    private ImageView[] dots;
 
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
@@ -83,46 +73,7 @@ public class PanelSettings extends PreferenceFragment implements RadioGroup.OnCh
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState) {
-	final View rootView = inflater.inflate(R.layout.panel_modes, container, false);
-
-	viewPager = (ViewPager) rootView.findViewById(R.id.viewPager);
-        sliderDotspanel = (LinearLayout) rootView.findViewById(R.id.SliderDots);
-        ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(getActivity());
-        viewPager.setAdapter(viewPagerAdapter);
-
-        dotscount = viewPagerAdapter.getCount();
-        dots = new ImageView[dotscount];
-
-        for(int i = 0; i < dotscount; i++){
-            dots[i] = new ImageView(getActivity());
-            dots[i].setImageDrawable(ContextCompat.getDrawable(getActivity().getApplicationContext(), R.drawable.inactive_dot));
-
-            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
-            params.setMargins(8, 0, 8, 0);
-            sliderDotspanel.addView(dots[i], params);
-        }
-        dots[0].setImageDrawable(ContextCompat.getDrawable(getActivity().getApplicationContext(), R.drawable.active_dot));
-
-        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
-
-            @Override
-            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-            }
-
-            @Override
-            public void onPageSelected(int position) {
-
-                for(int i = 0; i< dotscount; i++){
-                    dots[i].setImageDrawable(ContextCompat.getDrawable(getActivity().getApplicationContext(), R.drawable.inactive_dot));
-                }
-                dots[position].setImageDrawable(ContextCompat.getDrawable(getActivity().getApplicationContext(), R.drawable.active_dot));
-            }
-
-            @Override
-            public void onPageScrollStateChanged(int state) {
-            }
-        });
-	return rootView;
+        return inflater.inflate(R.layout.panel_modes, container, false);
     }
 
     @Override
